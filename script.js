c68e34305439f95228a4c4b6a4e721c1c365ba71
sophiacontesini <sophiamarques214@gmail.com>
2021-12-16 10:20:29 -0300

    requisitos 6 e 7 ok

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 
+//linkando os elementos
+let black = document.querySelector("#black");
+let blue = document.querySelector("#blue");
+let purple = document.querySelector("#purple");
+let pink = document.querySelector("#pink");
+
+//para mudar quando seleciona 
+function change(event) {
+  let changeSelection = document.querySelector(".selected");
+  changeSelection.classList.remove("selected");
+  event.target.classList.add("selected");
+}
+
+//configurando os cliques
+// addEventListenner 
+black.addEventListener("click", change);
+blue.addEventListener("click", change);
+purple.addEventListener("click", change);
+pink.addEventListener("click", change);
+
 
