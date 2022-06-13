da55c120ad47dfe481f24029684c7fec3e458410
sophiacontesini <sophiamarques214@gmail.com>
2022-06-13 14:29:11 -0300

    add css

--- script.js
+++ script.js
@@ -1,56 +1,49 @@
+// linkando os elementos
+const black = document.querySelector('#black');
+const blue = document.querySelector('#blue');
+const purple = document.querySelector('#purple');
+const pink = document.querySelector('#pink');
 
-//linkando os elementos
-let black = document.querySelector("#black");
-let blue = document.querySelector("#blue");
-let purple = document.querySelector("#purple");
-let pink = document.querySelector("#pink");
+black.style.backgroundColor = 'black';
+purple.style.backgroundColor = 'rgba(104, 38, 104, 0.871)';
+blue.style.backgroundColor = 'cadetblue';
+pink.style.backgroundColor = 'pink';
 
-black.style.backgroundColor = "black";
-purple.style.backgroundColor = "purple";
-blue.style.backgroundColor = "blue";
-pink.style.backgroundColor = "pink";
-
-//7. para mudar quando seleciona  
+// 7. para mudar quando seleciona
 function change(event) {
-  let changeSelection = document.querySelector(".selected");
-  changeSelection.classList.remove("selected");
-  event.target.classList.add("selected");
+  const changeSelection = document.querySelector('.selected');
+  changeSelection.classList.remove('selected');
+  event.target.classList.add('selected');
 }
 
-//configurando os cliques
+// configurando os cliques
 // addEventListenner (escutador de eventos)
-black.addEventListener("click", change);
-blue.addEventListener("click", change);
-purple.addEventListener("click", change);
-pink.addEventListener("click", change);
+black.addEventListener('click', change);
+purple.addEventListener('click', change);
+pink.addEventListener('click', change);
 
-//8. Preencher o pixel branco com a cor selecionada na paleta
-//Recebi ajuda da Débora Serra - Turma 19/Tribo B
+// 8. Preencher o pixel branco com a cor selecionada na paleta
+// Recebi ajuda da Débora Serra - Turma 19/Tribo B
 function colorir(e) {
-  let pintar = document.querySelector(".selected").style.backgroundColor;
+  const pintar = document.querySelector('.selected').style.backgroundColor;
   if (e.target.style.backgroundColor !== pintar) {
     e.target.style.backgroundColor = pintar;
-  } else if (e.target.style.backgroundColor === pintar){
-    e.target.style.backgroundColor = "white";
+  } else if (e.target.style.backgroundColor === pintar) {
+    e.target.style.backgroundColor = 'white';
   }
-  
 }
-let pixel = document.querySelectorAll(".pixel");
-for (let p = 0; p < pixel.length; p +=1){
-  pixel[p].addEventListener("click", colorir);
+const pixel = document.querySelectorAll('.pixel');
+for (let p = 0; p < pixel.length; p += 1) {
+  pixel[p].addEventListener('click', colorir);
 }
 
-//9. Botão Limpar
-//Recebi ajuda da Débora Serra - Turma 19/Tribo B
-let botaoLimpar = document.getElementById("clear-board");
+// 9. Botão Limpar
+// Recebi ajuda da Débora Serra - Turma 19/Tribo B
+const botaoLimpar = document.getElementById('clear-board');
 function limpar() {
-  let limp = document.querySelectorAll(".pixel");
-  for (let i = 0; i < limp.length; i+=1){
-    limp[i].style.backgroundColor = "white";
+  const limp = document.querySelectorAll('.pixel');
+  for (let i = 0; i < limp.length; i += 1) {
+    limp[i].style.backgroundColor = 'white';
   }
 }
-botaoLimpar.addEventListener("click", limpar);
-
-
-
-
+botaoLimpar.addEventListener('click', limpar);
